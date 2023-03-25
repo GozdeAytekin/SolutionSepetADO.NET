3733288ae5d7954a1d53ae402bfc89eecbc01ac3
gozdeaytekin9@gmail.com <gozdeaytekin9@gmail.com>
2023-03-25 23:19:19 +0300

    Add project files.

---
+++ SolutionSepet.UI/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolutionSepet.UI
+{
+
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+        string musteriId;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("server=.;Database=Northwind;Integrated Security = True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert Customers(CustomerID,CompanyName) values (@Id,@companyName)", conn);
+            cmd.Parameters.AddWithValue("@Id",sirketId.Text);
+            cmd.Parameters.AddWithValue("@companyName", companyName.Text);
+            musteriId = sirketId.Text;
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Form2 frm= new Form2(musteriId);
+            frm.Show();
+
+        }
+       
+        }
+    
+}
